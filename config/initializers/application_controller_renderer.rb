01e5a9bde68045a6ae8980b5bb8af742b9ecb538
m0rdr3-d <dammyaro@gmail.com>
2018-11-22 03:32:27 +0000

    init

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -9,3 +9,8 @@
 
 require 'block_io'
 BlockIo.set_options :api_key=> '85bf-a839-d4d6-58be', :pin => '781227781228', :version => 2
+
+production:
+  cloud_name: "dsegcukxy"
+  api_key: "123837668317786"
+  api_secret: "8E7ARHlp1ZK2ObBpB5dNtgDUKJQ"
