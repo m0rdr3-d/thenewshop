a53a587dce2c43dacc748fdc3a566b34d37aaa30
root <root@server11079.com>
2018-12-29 23:07:05 -0500

    fix orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -73,6 +73,6 @@ class OrdersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def order_params
-      params.require(:order).permit(:name, :quan tity, :email, :location, :price)
+      params.require(:order).permit(:name, :quantity, :email, :location, :price)
     end
 end
